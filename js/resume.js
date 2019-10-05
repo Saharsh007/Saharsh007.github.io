21c978a4fde4b0e13db019f2865126a8cd87a78c
saharsh007 <saharshsonu40@gmail.com>
2019-10-06 00:23:33 +0530

    certificates uploaded

--- js/resume.js
+++ js/resume.js
@@ -1,6 +1,7 @@
 (function($) {
   "use strict"; // Start of use strict
 
+
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
@@ -26,3 +27,4 @@
   });
 
 })(jQuery); // End of use strict
+
